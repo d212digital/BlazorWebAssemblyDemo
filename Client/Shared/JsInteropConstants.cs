f936649a2c6e3a2641ce183bee170f8fc38c4d5b
David Adamson <david@212digital.co.uk>
2022-03-17 10:24:18 +0000

    adding AppState to app and counter

---
+++ Client/Shared/JsInteropConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorDemo.Client.Shared
+{
+    public class JsInteropConstants
+    {
+        public const string FuncPrefix = "blazorDemo";
+
+        public const string GetSessionStorage = $"{FuncPrefix}.getSessionStorage";
+
+        public const string SetSessionStorage = $"{FuncPrefix}.setSessionStorage";
+
+    }
+}
---
+++ Client/wwwroot/scripts/BlazorDemo.js
@@ -0,0 +1,9 @@
+﻿var blazorDemo = {};
+
+blazorDemo.getSessionStorage = function (key) {
+    return sessionStorage.getItem(key);
+}
+
+blazorDemo.setSessionStorage = function (key, data) {
+    return sessionStorage.setItem(key, data);
+}
